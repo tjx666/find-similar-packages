3bcec2027f50e218299839d2cbdd76605379412f
changfeng <changfeng@gaoding.com>
2023-02-05 01:37:46 +0800

    feat: add options --ignore, --concurrency

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
+import { isAbsolute, resolve } from 'node:path';
+
 import { findWorkspacePackagesNoCheck } from '@pnpm/find-workspace-packages';
 import axios from 'axios';
+import { program } from 'commander';
 import pLimit from 'p-limit';
+
+import packageJSON from './package.json';
 
 async function getSimilarPackages(packageName: string) {
     const searchUrl =
@@ -10,12 +15,16 @@ async function getSimilarPackages(packageName: string) {
     return similarPackageNames;
 }
 
-async function getProjectDeps(projectRoot: string) {
+async function getProjectDeps(projectRoot: string, ignorePackagesNameRegexp?: RegExp) {
     const packages = await findWorkspacePackagesNoCheck(projectRoot);
     const deps = new Set<string>();
     for (const pkg of packages) {
         const { name, dependencies } = pkg.manifest;
-        if (name && !/^@scripts\//.test(name) && dependencies) {
+        if (
+            name &&
+            !(ignorePackagesNameRegexp !== undefined && ignorePackagesNameRegexp.test(name)) &&
+            dependencies
+        ) {
             for (const dep of Object.keys(dependencies)) {
                 deps.add(dep);
             }
@@ -31,14 +40,28 @@ function isSubSet(subSet: Set<string>, parentSet: Set<string>) {
     return true;
 }
 
-async function main() {
-    const projectRoot = process.argv[2] ?? process.cwd();
-    const deps = await getProjectDeps(projectRoot);
+interface Options {
+    ignorePackagesNameRegexp?: RegExp;
+    concurrency: number;
+}
+
+async function findSimilarPackages(args: string[], options: Options) {
+    let projectRoot: string;
+    if (args[0] !== undefined) {
+        projectRoot = args[0];
+        if (!isAbsolute(projectRoot)) {
+            projectRoot = resolve(projectRoot);
+        }
+    } else {
+        projectRoot = process.cwd();
+    }
+
+    const deps = await getProjectDeps(projectRoot, options.ignorePackagesNameRegexp);
     const depSet = new Set(deps);
     const depToSetMap = new Map<string, Set<string> | undefined>(
         deps.map((dep) => [dep, undefined]),
     );
-    const limit = pLimit(50);
+    const limit = pLimit(options.concurrency);
     await Promise.all(
         deps.map((dep) =>
             limit(async () => {
@@ -79,4 +102,20 @@ async function main() {
     }
 }
 
-main();
+(async function main() {
+    program
+        .option('-i, --ignore <regexp>', 'a regexp to specify which packages should be ignore')
+        .option('-c, --concurrency <number>', 'concurrency count to check packages', '50')
+        .version(packageJSON.version)
+        .name('find-similar-packages')
+        .usage('[options] /path/to/pnpm/monorepo/projectRoot');
+    program.parse();
+
+    const options = program.opts();
+    findSimilarPackages(program.args, {
+        ignorePackagesNameRegexp:
+            // eslint-disable-next-line security/detect-non-literal-regexp
+            options.ignore !== undefined ? new RegExp(options.ignore) : undefined,
+        concurrency: Number.parseInt(options.concurrency),
+    });
+})();
